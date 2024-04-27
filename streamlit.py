1b13c402e0188ceb3befde05dee9ca6cd7c0bc18
hakimguenther <59147016+hakimguenther@users.noreply.github.com>
2024-04-27 19:17:48 +0000

    a

--- streamlit.py
+++ streamlit.py
@@ -13,28 +13,19 @@ from mazegame import MazeGameEnv
 
 st.set_page_config(
     layout="wide",
-    page_title="Pygame-CE Drawing Sandbox",
+    page_title="RL",
     page_icon="🎨",
 )
 
 
 
-st.title("Pygame-CE Drawing Sandbox 🎨:snake:")
-st.write("Experiment and test out Pygame (Community Edition) static drawing functions with a real-time preview.")
-st.markdown("In this sandboxed environment, `pygame`, `pygame.gfxdraw`, and `numpy` are imported and a limited set of Python `builtins` are available for use.")
-st.markdown("The display surface variable is named `SCREEN`, it's size is `(800, 600)` and is accessible via `SCREEN_SIZE`, and uploaded image surface accessible via `IMAGE_SURFACE`.")
-st.markdown("Feel free to tag or message me on the [Pygame-CE Discord](https://discord.com/channels/772505616680878080/772505616680878083) `@Djo` for any questions or suggestions. The source code for this Streamlit app is available on GitHub by following the icon link at the top right of the page.")
-
-
+st.title("RL Simulator")
 
 status = st.container()
-col1, col2 = st.columns(2)
 placeholder = st.empty()
 
 
 try:
-    exec("st.title(\"Pygame-CE Drawing Sandbox 🎨:snake:\")")
-    status.success('Code executed successfully')
     # Create environment
     #env = MazeGameEnv(maze=maze, render_mode='human')
     env = MazeGameEnv(size=[10,10], render_mode='human')
